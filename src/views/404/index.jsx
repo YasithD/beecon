8f8988fcce2fef36ed3367c9c218379d7eee6d68
Yasith <yasith.18@cse.mrt.ac.lk>
2021-03-27 14:02:32 +0530

    Bootstrap CDN added

--- src/views/404/index.jsx
+++ src/views/404/index.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import {
-    CCard,
-    CCardBody,
-    CRow,
-    CCol
-} from "@coreui/react" 
+import React from "react";
+import { CContainer, CRow, CCol, CImg, CButton } from "@coreui/react";
+import img from "../../images/404.svg"
 
 const Error = () => {
-    return (
-        <CCard>
-            <CCardBody>
-                <CRow>
-                    <CCol sm="3" style={{backgroundColor: "red"}}> Col 1 </CCol>
-                    <CCol sm="9" style={{backgroundColor: "blue"}}> Col 2 </CCol>
-                </CRow>
-            </CCardBody>
-        </CCard>
-    )
-}
+  return (
+    <CContainer>
+      <CRow className="justify-content-center mt-5">
+        <CCol sm="8" className="">
+          <CImg 
+            src={img}
+            fluid
+            width="95%"
+          />
+        </CCol>
+      </CRow>
+      <CRow className="justify-content-center mt-5">
+          <CCol sm="10">
+            <div style={{fontFamily: "Poppins"}}>
+                We can't seem to find that
+            </div>
+          </CCol>
+      </CRow>
+      <CRow className="justify-content-center mt-5">
+          <CCol sm="10">
+            <div style={{fontFamily: "Poppins"}}>
+                The page you're looking for doesn't exist or has been moved
+            </div>
+          </CCol>
+      </CRow>
+      <CRow>
+          <button> Back To Dashboard </button>
+      </CRow>
+    </CContainer>
+  );
+};
 
-export default Error
-
+export default Error;
